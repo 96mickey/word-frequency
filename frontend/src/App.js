6f37a394329341f20ec753409c3cc96d603dba5d
Mayank <mayankworklabs@gmail.com>
2019-03-11 12:57:51 +0530

    change endpoint

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -26,7 +26,11 @@ class App extends Component {
       });
     else {
       this.setState({ loading: true });
-      fetch(`http://localhost:3001/api/user/getfrequency/${this.state.number}`)
+      fetch(
+        `http://ec2-18-218-14-83.us-east-2.compute.amazonaws.com:3001/api/user/getfrequency/${
+          this.state.number
+        }`
+      )
         .then(res => res.json())
         .then(res => {
           if (res.status === "success")
